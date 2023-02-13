e6ed141f84e8fed84c78c0a828c2cb2515056628
psvel332~ <sakthivelpk2002@gmail.com>
2023-02-13 10:03:54 +0530

    feb13

--- js/main.js
+++ js/main.js
@@ -315,3 +315,25 @@
 
 
 })(jQuery);
+// photos source
+
+$(document).ready(function() {
+    $('.container--gallery').magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      mainClass: 'mfp-with-zoom mfp-img-mobile',
+      image: {
+        verticalFit: true,
+      },
+      gallery: {
+        enabled: true
+      },
+      zoom: {
+        enabled: true,
+        duration: 230,
+        opener: function(element) {
+          return element.find('img');
+        }
+      }
+    });
+  });
